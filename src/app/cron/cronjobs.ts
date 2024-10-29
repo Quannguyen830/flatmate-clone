d7aa180fd4e19c593c566478f1efd07bfd276fb3
vietquanRMIT <nguyenvietquan512@gmail.com>
2024-10-29 22:07:58 +1100

    Review error

--- src/app/cron/cronjobs.ts
+++ src/app/cron/cronjobs.ts
@@ -3,6 +3,6 @@ import { Cron } from "croner";
 export function initCronJobs() {
     const job = new Cron('0 2 * * *', { timezone: "UTC" }, async () => {
         console.log("Starting daily API fetch...");
-        await fetch(`${process.env.NEXTAUTH_URL}/api/scrapeList`)
+        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/scrapeList`)
     });
 }
